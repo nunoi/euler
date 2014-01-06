044569f4262b2cf371a59109fd107ee8c5d65356
Nuno C. Inácio <nuno@nunoinacio.com>
2014-01-06 17:31:46 +0000

    Initial solution to problem 5 - not working

---
+++ p005/p005.c
@@ -0,0 +1,23 @@
+/*
+ *
+ * What is the smallest number evenly divisible by
+ * all numbers from 1 to 20 ?
+ *
+ */
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int i = 20;
+    int divisible = 0;
+
+    while (!divisible) {
+        i++;
+/*        for (j = 20; j > 1; j--) {
+            if (i % j != 0) {
+                break; 
+            }
+        } */
+    }
+}
+
